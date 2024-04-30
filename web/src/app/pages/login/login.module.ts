d05f3e92149eacd34fb2b277dd52c2ca90a6c6e9
joaomatozinhos <jpmatozinhos@gmail.com>
2024-04-30 19:17:37 -0300

    criação de componentes

--- web/src/app/pages/login/login.module.ts
+++ web/src/app/pages/login/login.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RedefinicaoSenhaComponent } from './redefinicao-senha/redefinicao-senha.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RedefinicaoSenhaComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ web/src/app/pages/login/redefinicao-senha/redefinicao-senha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redefinicao-senha',
+  templateUrl: './redefinicao-senha.component.html',
+  styleUrls: ['./redefinicao-senha.component.css']
+})
+export class RedefinicaoSenhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
